a5c61ecb02023dff5404ab50d026abf1089ccd45
Ahmad Elgabri <ahmedlg46@gmail.com>
2020-06-15 05:07:35 +0200

    fixed dates bug

--- src/views/Orders/Orders.js
+++ src/views/Orders/Orders.js
@@ -75,12 +75,15 @@ class Orders extends Component {
 
   fetchOrders = async (skip, limit) => {
     this.setState({ordersLoading: true})
+    let start = this.state.startDate
+    let end = this.state.endDate
+
     try {
-      let res = await api.get(`${baseURL}/order?skip=${skip}&limit=${limit}&startDate=${convertCalendarDate(this.state.startDate)}&endDate=${convertCalendarDate(this.state.endDate)}`)
+      let res = await api.get(`${baseURL}/order?skip=${skip}&limit=${limit}&startDate=${start}&endDate=${end}&sort=createdAt DESC`)
       let orders = res.data.results
       let count = res.data.count
       let pages = Math.ceil(count/30)
-      this.setState({orders, pages, ordersLoading: false, error: false,})
+      this.setState({orders, pages, ordersLoading: false, error: false, ordersCount: count})
     }
     catch(err) {
       this.setState({ordersLoading: false, error: true})
@@ -149,7 +152,7 @@ class Orders extends Component {
     this.setState({endDate: val},() => this.fetchOrders(skip, limit))
   }
   render() {
-    let { orders, pages, page, ordersLoading, error, panelExpanded, orderItems, buttonLoading, endDate, startDate } = this.state
+    let { orders, pages, page, ordersLoading, error, panelExpanded, orderItems, buttonLoading, endDate, startDate, ordersCount } = this.state
     return (
       <Base>
       <Flex justifyContent="" width={1}>
@@ -166,98 +169,108 @@ class Orders extends Component {
       </Flex>
         {
           error?<h3>an error has occured</h3> : 
-            ordersLoading? <CircularProgress /> : orders&&orders.map((order, index) => {
-              return (
-                <div key={order.id} className="root">
-                 <ExpansionPanel width={1} expanded={panelExpanded===index} onChange={() => this.setPanelExpanded(index, order)}>
-                   <ExpansionPanelSummary
-                     expandIcon={<ExpandMoreIcon />}
-                     aria-controls="panel1bh-content"
-                     id="panel1bh-header"
-                   >
-                     <Typography className={classes.heading}>Order #{order.createdAt}</Typography>
-                     <Typography className={classes.heading}>( {order.status} )</Typography>
-                   </ExpansionPanelSummary>
-                   <ExpansionPanelDetails>
-                     <Typography>
-                       <Typography className={classes.secondaryHeading}>
-                         <b>Client: </b>{order.user&&order.user.name}
-                       </Typography>
-                       <Typography>
-                         <b>Phone Number: </b> {order.user&&order.user.phoneNumber}
-                       </Typography>
-                       <Typography>
-                         <b>Email: </b> {order.auth&&order.auth.email}
-                       </Typography>
-                       <Typography>
-                         <b>Order status: </b> {order.status}
-                       </Typography>
-                       <Typography>
-                         <b>Payment method: </b> {
-                         order.method == 'we-accept' ? order.method : (
-                           <>
-                             { order.method +' '} 
-                             {
-                               order.status!="completed"? 
-                             <Button variant="contained" onClick={() => this.completeCashPayment(order)} color="primary">
-                               {!buttonLoading? 'Complete Order' : 'Loading...'}
-                             </Button> :
-                             ''
-                             }
-                           </>
-                         )
-                         }
-                       </Typography>
-                       <Typography>
-                         <b>Amount: </b> {order.amount || '0'} EGP
-                       </Typography>
-                       <Typography>
-                         <b>Delivery Time: </b> {order.orderDeliveryTime || '0'} EGP
-                       </Typography>
-                     </Typography>
-                   </ExpansionPanelDetails>
-                   <ExpansionPanelDetails>
-                     <Typography>
-                       <Typography>
-                         <b>Order Items:</b> 
-                       </Typography>
-                       <Typography>
-                         {orderItems&&orderItems.map(item => <>
-                         <p>{item.quantity || ''} x {item.nameEnglish || 'no name'} <b>{item.price*item.quantity || ''} EGP</b> </p>
-                         </>
-                         )}
-                       </Typography>
-                     </Typography>
-                   </ExpansionPanelDetails>
-                   {(order&&(order.street||order.landmark||order.floor||order.city||order.building))&& 
+            ordersLoading? <CircularProgress /> : 
+            <>
+            <h5>Orders Count: {ordersCount}</h5>
+            {
+              orders&&orders.map((order, index) => {
+                return (
+                  <div key={order.id} className="root">
+                   <ExpansionPanel width={1} expanded={panelExpanded===index} onChange={() => this.setPanelExpanded(index, order)}>
+                     <ExpansionPanelSummary
+                       expandIcon={<ExpandMoreIcon />}
+                       aria-controls="panel1bh-content"
+                       id="panel1bh-header"
+                     >
+                       <Typography className={classes.heading}>Order #{order.createdAt}</Typography>
+                       <Typography className={classes.heading}>( {order.status} )</Typography>
+                     </ExpansionPanelSummary>
                      <ExpansionPanelDetails>
                        <Typography>
-                         <Typography>
-                           <b>Address Details:</b> 
+                         <Typography className={classes.secondaryHeading}>
+                           <b>Order received at: {new Date(order.createdAt).toLocaleTimeString().toString()}</b>
+                         </Typography>
+                         <Typography className={classes.secondaryHeading}>
+                           <b>Client: </b>{order.user&&order.user.name}
                          </Typography>
                          <Typography>
-                           Street: {order.street || ''}
+                           <b>Phone Number: </b> {order.user&&order.user.phoneNumber}
                          </Typography>
                          <Typography>
-                           Building: {order.building || ''}
+                           <b>Email: </b> {order.auth&&order.auth.email}
                          </Typography>
                          <Typography>
-                           Floor: {order.floor || ''}
+                           <b>Order status: </b> {order.status}
                          </Typography>
                          <Typography>
-                           apartment: {order.appartment || ''}
+                           <b>Payment method: </b> {
+                           order.method == 'we-accept' ? order.method : (
+                             <>
+                               { order.method +' '} 
+                               {
+                                 order.status!="completed"? 
+                               <Button variant="contained" onClick={() => this.completeCashPayment(order)} color="primary">
+                                 {!buttonLoading? 'Complete Order' : 'Loading...'}
+                               </Button> :
+                               ''
+                               }
+                             </>
+                           )
+                           }
                          </Typography>
                          <Typography>
-                           landmark: {order.landmark || ''}
+                           <b>Amount: </b> {order.amount || '0'} EGP
+                         </Typography>
+                         <Typography>
+                           <b>Delivery Time: </b> {order.orderDeliveryTime || '0'} EGP
                          </Typography>
                        </Typography>
                      </ExpansionPanelDetails>
-                   }
-                 </ExpansionPanel>
-                 </div>
-              ) 
-             //  <Order order={order} />
-             })
+                     <ExpansionPanelDetails>
+                       <Typography>
+                         <Typography>
+                           <b>Order Items:</b> 
+                         </Typography>
+                         <Typography>
+                           {orderItems&&orderItems.map(item => <>
+                           <p>{item.quantity || ''} x {item.nameEnglish || 'no name'} <b>{item.price*item.quantity || ''} EGP</b> </p>
+                           </>
+                           )}
+                         </Typography>
+                       </Typography>
+                     </ExpansionPanelDetails>
+                     {(order&&(order.street||order.landmark||order.floor||order.city||order.building))&& 
+                       <ExpansionPanelDetails>
+                         <Typography>
+                           <Typography>
+                             <b>Address Details:</b> 
+                           </Typography>
+                           <Typography>
+                             Street: {order.street || ''}
+                           </Typography>
+                           <Typography>
+                             Building: {order.building || ''}
+                           </Typography>
+                           <Typography>
+                             Floor: {order.floor || ''}
+                           </Typography>
+                           <Typography>
+                             apartment: {order.appartment || ''}
+                           </Typography>
+                           <Typography>
+                             landmark: {order.landmark || ''}
+                           </Typography>
+                         </Typography>
+                       </ExpansionPanelDetails>
+                     }
+                   </ExpansionPanel>
+                   </div>
+                ) 
+               //  <Order order={order} />
+               })
+            }
+            </>
+            
           
         }
         
@@ -271,3 +284,102 @@ class Orders extends Component {
 
 
 export default Orders;
+
+
+function convertToCSV(objArray) {
+  var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+  var str = '';
+
+  for (var i = 0; i < array.length; i++) {
+      var line = '';
+      for (var index in array[i]) {
+          if (line != '') line += ','
+
+          line += array[i][index];
+      }
+
+      str += line + '\r\n';
+  }
+
+  return str;
+}
+
+function exportCSVFile(headers, items, fileTitle) {
+  if (headers) {
+      items.unshift(headers);
+  }
+
+  // Convert Object to JSON
+  var jsonObject = JSON.stringify(items);
+
+  var csv = this.convertToCSV(jsonObject);
+
+  var exportedFilenmae = fileTitle + '.csv' || 'export.csv';
+
+  var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+  if (navigator.msSaveBlob) { // IE 10+
+      navigator.msSaveBlob(blob, exportedFilenmae);
+  } else {
+      var link = document.createElement("a");
+      if (link.download !== undefined) { // feature detection
+          // Browsers that support HTML5 download attribute
+          var url = URL.createObjectURL(blob);
+          link.setAttribute("href", url);
+          link.setAttribute("download", exportedFilenmae);
+          link.style.visibility = 'hidden';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+      }
+  }
+}
+
+
+// function download(){
+// var headers = {
+//     model: 'Phone Model'.replace(/,/g, ''), // remove commas to avoid errors
+//     chargers: "Chargers",
+//     cases: "Cases",
+//     earphones: "Earphones"
+// };
+
+// itemsNotFormatted = [
+//     {
+//         model: 'Samsung S7',
+//         chargers: '55',
+//         cases: '56',
+//         earphones: '57',
+//         scratched: '2'
+//     },
+//     {
+//         model: 'Pixel XL',
+//         chargers: '77',
+//         cases: '78',
+//         earphones: '79',
+//         scratched: '4'
+//     },
+//     {
+//         model: 'iPhone 7',
+//         chargers: '88',
+//         cases: '89',
+//         earphones: '90',
+//         scratched: '6'
+//     }
+// ];
+
+// var itemsFormatted = [];
+
+// // format the data
+// itemsNotFormatted.forEach((item) => {
+//     itemsFormatted.push({
+//         model: item.model.replace(/,/g, ''), // remove commas to avoid errors,
+//         chargers: item.chargers,
+//         cases: item.cases,
+//         earphones: item.earphones
+//     });
+// });
+
+// var fileTitle = 'orders'; // or 'my-unique-title'
+
+// exportCSVFile(headers, itemsFormatted, fileTitle); // call the exportCSVFile() function to process the JSON and trigger the download
+// }
